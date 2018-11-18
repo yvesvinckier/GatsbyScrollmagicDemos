15743a1444f44effc6ddb17831a8a8e3665984a8
Yves Vinckier <yves.vinckier@live.fr>
2018-11-19 00:08:19 +0100

    all scenes

--- src/pages/pen.js
+++ src/pages/pen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { TweenMax } from "gsap/all";
+import { TweenMax, Linear } from "gsap/all";
+import ScrollMagic from "../utils/ScrollMagic"; // my own wrapper for scrollmagic that includes greensock
 import imgPen from '../assets/images/img_pen-landscape.png'
 
 class Pen extends Component {
@@ -13,6 +14,85 @@ class Pen extends Component {
     componentDidMount() {
         // move the pen body up to connect with the first part
         TweenMax.set('.part3', { y: -572 })
+        // hide all headings and text
+        // TweenMax.set(['.parts h2, .parts p'], { autoAlpha: 0 })
+        // create a tween that will move the pen body back to its original css position
+        const bodyToStart = TweenMax.to('.part3', 1, {
+            y: 0,
+            ease: Linear.easeNone,
+        })
+        // Init ScrollMagic Controller
+        const controller = new ScrollMagic.Controller();
+        // Create a Scene 1 - move pen body back to start - HOW?
+        const bodyToStartScene = new ScrollMagic.Scene({
+            triggerElement: this.part1Trigger,
+            triggerHook: 1,
+            offset: 287,
+            duration: 572
+        })
+            .setTween(bodyToStart)
+            .addTo(controller);
+
+        // move 4 and 5 down, to connect with part 6 - HOW?
+        // TweenMax.set('.part5', {y: 120});
+        // TweenMax.set('.part4', {y: 289});
+
+        // move part 6 to connect with the body
+        TweenMax.set('.part6', { y: -846 });
+
+        // move 4 and 5 to connect with part 6 again
+        TweenMax.set('.part5', { y: -726 }); /* 120 - 846 */
+        TweenMax.set('.part4', { y: -557 }); /* 289 - 846 */
+
+        // Create scene for part 6 - move part 6 back to start
+        const p6ToStart = new TweenMax.to('.part6', 1, { y: 0, ease: Linear.easeNone });
+
+        const p6ToStartScene = new ScrollMagic.Scene({
+            triggerElement: this.part1Trigger,
+            triggerHook: 1,
+            duration: 846,
+            offset: 1250
+        })
+            .setTween(p6ToStart)
+            // .addIndicators()
+            .addTo(controller);
+
+        // Create scene for part 5 - move part 5 back to start
+        const p5ToStart = new TweenMax.to('.part5', 1, { y: 0, ease: Linear.easeNone });
+
+        const p5ToStartScene = new ScrollMagic.Scene({
+            triggerElement: this.part1Trigger,
+            triggerHook: 1,
+            duration: 726,
+            offset: 1250
+        })
+            .setTween(p5ToStart)
+            .addTo(controller);
+
+        // Create scene for part 4 - move part 4 back to start
+        const p4ToStart = new TweenMax.to('.part4', 1, { y: 0, ease: Linear.easeNone });
+
+        const p4ToStartScene = new ScrollMagic.Scene({
+            triggerElement: this.part1Trigger,
+            triggerHook: 1,
+            duration: 557,
+            offset: 1250
+        })
+            .setTween(p4ToStart)
+            .addTo(controller);
+
+        // Loop through all parts and add a class .fade-in
+        const arrayLi = document.getElementsByClassName('parts')[0].getElementsByTagName("li");
+        console.log(arrayLi)
+        // [...arrayLi].map((item) => {
+        //     const scene = new ScrollMagic.Scene({
+        //         triggerElement: item,
+        //         triggerHook: 0.55
+        //     })
+        //         .setClassToggle(item, 'fade-in')
+        //         .addTo(controller);
+        //     console.log(item);
+        // })
     }
     render() {
         return (
